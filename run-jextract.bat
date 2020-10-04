4ae7d48d2531afd1f4238c24bf3592931372b4bf
tornaia <tornai.andras.ede@gmail.com>
2020-10-04 13:54:54 +0200

    processenv

--- run-jextract.bat
+++ run-jextract.bat
@@ -4,4 +4,4 @@ SET JAVA_HOME=c:\Program Files\Java\jdk-16\
 REM jextract -C "--verbose" -d target\jextract\classes --target-package com.github.tornaia.wcs -I "C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\cppwinrt" "C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\um\cfapi.h"
 
 REM jextract --source -C "--verbose" -d src\main\java --target-package com.github.tornaia.cf.win.cfapi "C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\um\cfapi.h"
-jextract --source -C "--verbose" -d src\main\java --target-package com.github.tornaia.cf.win.sysinfoapi "C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\um\sysinfoapi.h"
+jextract --source -C "--verbose" -d src\main\java --target-package com.github.tornaia.cf.win.processenv "C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\um\processenv.h"
--- src/main/java/com/github/tornaia/cf/CFApp.java
+++ src/main/java/com/github/tornaia/cf/CFApp.java
@@ -1,11 +1,24 @@
 package com.github.tornaia.cf;
 
-import com.github.tornaia.cf.win.sysinfoapi.sysinfoapi_h;
+import com.github.tornaia.cf.win.cfapi.cfapi_h;
+import com.github.tornaia.cf.win.processenv.processenv_h;
+
+import java.lang.invoke.MethodHandle;
 
 public class CFApp {
 
-    public static void main(String[] args) {
-        int i = sysinfoapi_h.GetVersion();
-        System.out.println("version: " + i);
+    public static void main(String[] args) throws Throwable {
+        MethodHandle methodHandle1 = processenv_h.GetCurrentDirectoryW$MH();
+        Object[] objects = (Object[]) methodHandle1.invoke();
+        MethodHandle methodHandle = cfapi_h.GetSystemWindowsDirectoryW$MH();
+        Object invoke = methodHandle.invoke();
+
+        int majorVersion = cfapi_h.VER_MAJORVERSION();
+        int minorVersion = cfapi_h.VER_MINORVERSION();
+
+        System.out.println("majorVersion: " + majorVersion);
+        System.out.println("minorVersion: " + minorVersion);
+
+        int i1 = cfapi_h.VER_BUILDNUMBER();
     }
 }
