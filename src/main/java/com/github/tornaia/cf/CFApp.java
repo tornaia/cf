c46e2ed3211cec203c0650bbbb57b8599f9a1454
tornaia <tornai.andras.ede@gmail.com>
2020-10-06 18:44:11 +0200

    Minimal processenv

--- src/main/java/com/github/tornaia/cf/CFApp.java
+++ src/main/java/com/github/tornaia/cf/CFApp.java
@@ -1,24 +1,18 @@
 package com.github.tornaia.cf;
 
-import com.github.tornaia.cf.win.cfapi.cfapi_h;
+import com.github.tornaia.cf.win.processenv.C;
 import com.github.tornaia.cf.win.processenv.processenv_h;
-
-import java.lang.invoke.MethodHandle;
+import jdk.incubator.foreign.Addressable;
+import jdk.incubator.foreign.MemorySegment;
 
 public class CFApp {
 
-    public static void main(String[] args) throws Throwable {
-        MethodHandle methodHandle1 = processenv_h.GetCurrentDirectoryW$MH();
-        Object[] objects = (Object[]) methodHandle1.invoke();
-        MethodHandle methodHandle = cfapi_h.GetSystemWindowsDirectoryW$MH();
-        Object invoke = methodHandle.invoke();
-
-        int majorVersion = cfapi_h.VER_MAJORVERSION();
-        int minorVersion = cfapi_h.VER_MINORVERSION();
-
-        System.out.println("majorVersion: " + majorVersion);
-        System.out.println("minorVersion: " + minorVersion);
-
-        int i1 = cfapi_h.VER_BUILDNUMBER();
+    public static void main(String[] args) {
+        try (MemorySegment memorySegment = MemorySegment.allocateNative(256)) {
+            @C("LPWSTR") Addressable a = memorySegment.address();
+            int retval = processenv_h.GetWindowsDirectoryW(a, 260);
+            String getWindowsDirectoryW = new String(memorySegment.toByteArray());
+            System.out.println("GetWindowsDirectoryW: " + getWindowsDirectoryW);
+        }
     }
 }
