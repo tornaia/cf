77656dd4485ebc2fe1dde11009ff5a8692117071
tornaia <tornai.andras.ede@gmail.com>
2020-10-10 18:14:28 +0200

    Add extra memory to jextract

--- run-jextract.bat
+++ run-jextract.bat
@@ -1,6 +1,7 @@
 SET PATH=c:\Program Files\Java\jdk-16\bin\;%PATH%
 SET JAVA_HOME=c:\Program Files\Java\jdk-16\
 
-REM jextract -C "--verbose" -d target\jextract\classes --target-package com.github.tornaia.wcs -I "C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\cppwinrt" "C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\um\cfapi.h"
+REM jextract -C "--verbose" -J-Xmx16G -d target\jextract\classes --target-package com.github.tornaia.wcs -I "C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\cppwinrt" "C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\um\cfapi.h"
+REM jextract --source -C "--verbose" -J-Xmx16G -d src\main\java --target-package com.github.tornaia.cf.win.wsp.internal -I "C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\cppwinrt" "C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\winrt\windows.storage.provider.h"
 
-jextract --source -C "--verbose" -d src\main\java --target-package com.github.tornaia.cf.win.cfapi.internal "C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\um\cfapi.h"
+jextract --source -C "--verbose" -J-Xmx16G -d src\main\java --target-package com.github.tornaia.cf.win.cfapi.internal "C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\um\cfapi.h"
